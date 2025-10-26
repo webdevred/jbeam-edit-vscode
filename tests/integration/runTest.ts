d15a07c13948a00f396c063630dfaf5a6a1ff81a
debbie <148627186+webdevred@users.noreply.github.com>
2025-10-27 00:40:42 +0100

    tmp

--- tests/integration/runTest.ts
+++ tests/integration/runTest.ts
@@ -1,22 +1,20 @@
 import * as path from 'path';
-import {
-  runTests
-} from '@vscode/test-electron';
+import { runTests } from '@vscode/test-electron';
 
 async function main() {
-  try {
-    const extensionDevelopmentPath = path.resolve(__dirname, '../../');
-    const extensionTestsPath = path.resolve(__dirname, './integration/extension.test.ts');
+    try {
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../'); // root of extension
+        const extensionTestsPath = path.resolve(__dirname, 'extension.test.ts'); // fix here
 
-    await runTests({
-      extensionDevelopmentPath,
-      extensionTestsPath,
-      version: 'stable'
-    });
-  } catch (err) {
-    console.error('Failed to run tests', err);
-    process.exit(1);
-  }
+        await runTests({
+            extensionDevelopmentPath,
+            extensionTestsPath,
+            version: 'stable'
+        });
+    } catch (err) {
+        console.error('Failed to run tests', err);
+        process.exit(1);
+    }
 }
 
 main();
